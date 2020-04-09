03f1977466052d736c6d5e96e04dab70e3ea6807
Batyr Seven <batyrseven@gmail.com>
2020-04-09 12:49:47 +0300

    Added Order Frame

--- MergedApp/src/mergedapp/MenuJFrame.java
+++ MergedApp/src/mergedapp/MenuJFrame.java
@@ -99,7 +99,7 @@ public class MenuJFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-//        new OrderFrame().setVisible(true);
+        new OrderJFrame().setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
